85c382d88d7826ad668c5203a30651648434e6a6
inoen <inoeyt@gmail.com>
2018-04-03 20:51:06 +0900

    added second place displays the text corresponding to the text in first place.

--- quote.js
+++ quote.js
@@ -21,7 +21,9 @@ $("#button").on("click", function(){
     function(){
     $('.text').animate({
     'opacity' : '1'
-      })
+      }),
+      $('.text').text(data[random].name.text);
+      $('.quote_view').text(data[random].name.id);
       }
     );
     var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
@@ -34,7 +36,7 @@ $("#button").on("click", function(){
       $(".button").animate({
         backgroundColor: colors[color]
       });
-      $('.text').text(data[random].name);
+
       //  alert(data.name);
     },
     error: function(xhr, textStatus, errorThrown){
